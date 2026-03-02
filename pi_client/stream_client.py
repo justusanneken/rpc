3386de70f9ab39f95a422825fcb3014bb95e54f1
Justus Anneken <justusanneken@MacBookPro.localdomain>
2026-03-02 22:17:48 +0100

    Switch to picamera2 for CSI camera support

--- pi_client/stream_client.py
+++ pi_client/stream_client.py
@@ -1,29 +1,32 @@
 # This script runs on the Raspberry Pi
 # It captures frames from the camera and sends them to the server
 
-import cv2        # used to access the camera
-import requests   # used to send data over the internet
-import time       # used to wait a bit if something goes wrong
+from picamera2 import Picamera2  # built-in Pi camera library
+import requests                  # used to send data over the internet
+import time                      # used to wait if something goes wrong
+import cv2                       # used to convert the frame to JPEG
 
 # Change this to the IP address of your server
 SERVER_URL = "http://10.0.0.8:5050/upload"
 
-# Open the camera (0 = first camera connected)
-camera = cv2.VideoCapture(0)
+# Set up the camera
+camera = Picamera2()
+camera.configure(camera.create_preview_configuration())
+camera.start()
 
 print("Starting stream... press Ctrl+C to stop")
 
 while True:
     # Grab a single photo from the camera
-    success, frame = camera.read()
-
-    if not success:
-        print("Couldn't read from camera, trying again...")
-        time.sleep(1)
-        continue
+    frame = camera.capture_array()
 
     # Turn the photo into a JPEG so it's small enough to send fast
     success, jpeg = cv2.imencode('.jpg', frame)
+
+    if not success:
+        print("Couldn't encode frame, trying again...")
+        time.sleep(1)
+        continue
 
     # Send the photo to the server
     try:
